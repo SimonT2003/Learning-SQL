3144c7c55e68d6d58481f46691b14ae48276c021
Simon Tran <105665224+SimonT2003@users.noreply.github.com>
2023-05-14 17:43:23 -0700

    Update SQLQuery2_Select_From_Statements.sql

--- SQLQuery2_Select_From_Statements.sql
+++ SQLQuery2_Select_From_Statements.sql
@@ -3,40 +3,40 @@ Select Statment
 *, Top, Distinct, Count, As, Max, Min, Avg
 */
 
---SELECT * -- select everything
---FROM EmployeeDemographics
+SELECT * -- select everything
+FROM EmployeeDemographics
 
---SELECT FirstName
---FROM EmployeeDemographics
+SELECT FirstName
+FROM EmployeeDemographics
 
---SELECT FirstName, LastName
---FROM EmployeeDemographics
+SELECT FirstName, LastName
+FROM EmployeeDemographics
 
---SELECT TOP 5 *
---FROM EmployeeDemographics
+SELECT TOP 5 *
+FROM EmployeeDemographics
 
---SELECT DISTINCT(EmployeeID)
---FROM EmployeeDemographics
+SELECT DISTINCT(EmployeeID)
+FROM EmployeeDemographics
 
---SELECT DISTINCT(Gender)
---FROM EmployeeDemographics
+SELECT DISTINCT(Gender)
+FROM EmployeeDemographics
 
---SELECT COUNT(LastName) AS LastNameCount --'AS' names the column
---FROM EmployeeDemographics
+SELECT COUNT(LastName) AS LastNameCount --'AS' names the column
+FROM EmployeeDemographics
 
 
 
---SELECT *
---FROM EmployeeSalary
+SELECT *
+FROM EmployeeSalary
 
---SELECT MAX(Salary) AS Max_Salary
---FROM EmployeeSalary
+SELECT MAX(Salary) AS Max_Salary
+FROM EmployeeSalary
 
---SELECT MIN(Salary) AS Min_Salary
---FROM EmployeeSalary
+SELECT MIN(Salary) AS Min_Salary
+FROM EmployeeSalary
 
---SELECT AVG(Salary) AS AVG_Salary
---FROM EmployeeSalary
+SELECT AVG(Salary) AS AVG_Salary
+FROM EmployeeSalary
 
 
 
